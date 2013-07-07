0b838bb3a006107d5b1272f1a6dcf6ca12fdb2d5
Colin Carroll <colcarroll@gmail.com>
2013-07-07 13:27:54 -0500

    Fixed bug in editing, made app JSLint passable

--- app.js
+++ app.js
@@ -3,16 +3,18 @@
  * Module dependencies.
  */
 
-var express = require('express')
-  , routes = require('./routes')
-  , user = require('./routes/user')
-  , http = require('http')
-  , path = require('path')
-  , BookProvider = require('./bookprovider').BookProvider;
+"use strict";
+
+var express = require('express'),
+  routes = require('./routes'),
+  user = require('./routes/user'),
+  http = require('http'),
+  path = require('path'),
+  BookProvider = require('./bookprovider').BookProvider;
 
 var app = express();
 
-app.configure(function(){
+app.configure(function () {
   app.set('port', process.env.PORT || 3000);
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
@@ -26,66 +28,66 @@ app.configure(function(){
   app.use(express.static(path.join(__dirname, 'public')));
 });
 
-app.configure('development', function(){
+app.configure('development', function () {
   app.use(express.errorHandler());
 });
 
 var bookProvider = new BookProvider('localhost', 27017);
 
 //Routes
-app.get('/', function(req, res){
-    bookProvider.findAll(function(error, books) {
-      res.render('index', {
-        title: 'Books',
-        books: books
-      });
+app.get('/', function (req, res) {
+  bookProvider.findAll(function (error, books) {
+    res.render('index', {
+      title: 'Books',
+      books: books
     });
+  });
 });
 
-app.get('/book/new', function(req, res) {
+app.get('/book/new', function (req, res) {
   res.render('book_new', {
     title: "New Book"
   });
 });
 
 //save new book
-app.post('/book/new', function(req, res) {
+app.post('/book/new', function (req, res) {
   bookProvider.save({
     title: req.param('title'),
     author: req.param('author'),
     date_finished: req.param('date_finished'),
     thoughts: req.param('thoughts')
-  }, function(error, docs) {
+  }, function (error, docs) {
     res.redirect('/');
   });
 });
 
 //update a book
-app.get('/book/:id/edit', function(req, res) {
-  bookProvider.findById(req.param('_id'), function(error, a_book) {
+app.get('/book/:id/edit', function (req, res) {
+  bookProvider.findById(req.param('_id'), function (error, a_book) {
     res.render('book_edit', {title: "Edit Book", a_book: a_book});
   });
 });
 
 //save updated book
-app.post('/book/:id/edit', function(req, res){
-  bookProvider.update(req.param('_id'),{
+app.post('/book/:id/edit', function (req, res) {
+  bookProvider.update(req.param('_id'), {
     title: req.param('title'),
     author: req.param('author'),
     date_finished: req.param('date_finished'),
     thoughts: req.param('thoughts')
-  }, function(error, docs) {
+  }, function (error, docs) {
     res.redirect('/');
   });
 });
 
 //delete a book
-app.post('/book/:id/delete',function(req, res) {
-  bookProvider.delete(req.param('_id'), function(error, docs){
+app.post('/book/:id/delete', function (req, res) {
+  bookProvider.delete(req.param('_id'), function (error, docs) {
     res.redirect("/");
   });
 });
 
-http.createServer(app).listen(app.get('port'), function(){
+http.createServer(app).listen(app.get('port'), function () {
   console.log("Express server listening on port " + app.get('port'));
 });
--- bookprovider.js
+++ bookprovider.js
@@ -1,10 +1,12 @@
+"use strict";
+
 var Db = require('mongodb').Db;
 var Connection = require('mongodb').Connection;
 var Server = require('mongodb').Server;
 var BSON = require('mongodb').BSON;
 var ObjectID = require('mongodb').ObjectID;
 
-BookProvider = function(host, port) {
+var BookProvider = function (host, port) {
   this.db = new Db(
     "node-mongo-book",
     new Server(
@@ -14,93 +16,115 @@ BookProvider = function(host, port) {
       {auto_reconnect: true},
       {}
     )
-  )
-  this.db.open(function(){});
+  );
+  this.db.open(function () {});
 };
 
-BookProvider.prototype.getCollection = function(callback) {
-  this.db.collection("books", function(error, book_collection){
-    if ( error ) callback(error);
-    else callback(null, book_collection);
+BookProvider.prototype.getCollection = function (callback) {
+  this.db.collection("books", function (error, book_collection) {
+    if (error) {
+      callback(error);
+    } else {
+      callback(null, book_collection);
+    }
   });
 };
 
 //find all books
-BookProvider.prototype.findAll = function(callback) {
-  this.getCollection(function(error, book_collection) {
-    if( error ) callback(error);
-    else {
-      book_collection.find().toArray(function(error, results){
-        if( error ) callback(error);
-        else callback(null, results)
+BookProvider.prototype.findAll = function (callback) {
+  this.getCollection(function (error, book_collection) {
+    if (error) {
+      callback(error);
+    } else {
+      book_collection.find().toArray(function (error, results) {
+        if (error) {
+          callback(error);
+        } else {
+          callback(null, results);
+        }
       });
-    };
+    }
   });
 };
 
 //save new book
-BookProvider.prototype.save = function(books, callback) {
-  this.getCollection(function(error, book_collection) {
-    if( error ) callback(error);
-    else {
-      if( typeof(books.length) == "undefined") {
+BookProvider.prototype.save = function (books, callback) {
+  var i, book;
+  this.getCollection(function (error, book_collection) {
+    if (error) {
+      callback(error);
+    } else {
+      if (typeof (books.length) === "undefined") {
         books = [books];
-      };
-      for(var i = 0; i < books.length; i++ ) {
-        book = books[i]
+      }
+      for (i = 0; i < books.length; i += 1) {
+        book = books[i];
         book.created_at = new Date();
-      };
+      }
 
-      book_collection.insert(books, function() {
+      book_collection.insert(books, function () {
         callback(null, books);
       });
-    };
+    }
   });
 };
 
 //look up book by ID
-BookProvider.prototype.findById = function(id, callback) {
-  this.getCollection(function(error, book_collection) {
-    if( error ) callback(error);
-    else {
+BookProvider.prototype.findById = function (id, callback) {
+  this.getCollection(function (error, book_collection) {
+    if (error) {
+      callback(error);
+    } else {
       book_collection.findOne({
         _id: book_collection.db.bson_serializer.ObjectID.createFromHexString(id)
-        },
-        function(error, result) {
-          if( error) callback(error);
-          else callback(null, result);
-        });
+      }, function (error, result) {
+        if (error) {
+          callback(error);
+        } else {
+          callback(null, result);
+        }
+      });
     }
   });
 };
 
 //update a book
-BookProvider.prototype.update = function(bookId, books, callback){
-  this.getCollection(function(error, book_collection) {
-    if( error ) callback(error);
-    else {
+BookProvider.prototype.update = function (bookId, books, callback) {
+  this.getCollection(function (error, book_collection) {
+    if (error) {
+      callback(error);
+    } else {
       book_collection.update(
-        {_id:book_collection.db.bson_serializer.ObjectID.createFromHexString(bookId)},
+        {_id: book_collection.db.bson_serializer.ObjectID.createFromHexString(bookId)},
         books,
-        function(error, books) {
-          if( error ) callback(error);
-          else callback(null, books)
-        });
+        function (error, books) {
+          if (error) {
+            callback(error);
+          } else {
+            callback(null, books);
+          }
+        }
+      );
     }
   });
 };
 
 //delete a book
-BookProvider.prototype.delete = function(bookId, callback){
-  this.getCollection(function(error, book_collection) {
-    if( error ) callback(error);
-    else {
+BookProvider.prototype.delete = function (bookId, callback) {
+  this.getCollection(function (error, book_collection) {
+    if (error) {
+      callback(error);
+    } else {
       book_collection.remove(
-        {_id:book_collection.db.bson_serializer.ObjectID.createFromHexString(bookId)},
-        function(error, book){
-          if( error ) callback(error);
-          else callback(null, book);
-        });
+        {_id: book_collection.db.bson_serializer.ObjectID.createFromHexString(bookId)},
+        function (error, book) {
+          if (error) {
+            callback(error);
+          } else {
+            callback(null, book);
+          }
+        }
+      );
     }
   });
 };
